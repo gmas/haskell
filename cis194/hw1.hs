461c3880c7aa92633eee36d53b78a4f839a9aa07
George Masgras <gmasgras@gmail.com>
2014-06-24 23:37:18 -0700

    add rest of HW1 functions

--- cis194/hw1.hs
+++ cis194/hw1.hs
@@ -1,5 +1,21 @@
-toDigits nr = 
-  case compare nr 0 of
-    LT -> []
-    EQ -> []
-    GT -> [ read x :: Int | x <- ([c :[] | c <- show nr ]) ] 
+import Data.Char
+
+toDigits :: Integer -> [Integer]
+toDigits 0 = []
+toDigits nr = [ fromIntegral (digitToInt c) | c <- show nr, 
+                    isNumber c, nr > 0 ]
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev nr = reverse $ toDigits nr
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther (x:xs) = x : (2 * head xs) : doubleEveryOther (tail xs)
+
+sumDigits :: (Num a) => [a] -> a
+sumDigits [] = 0
+sumDigits (x:xs) = x + sumDigits xs
+
+validate :: Integer -> Bool
+validate nr  = mod (sumDigits $ doubleEveryOther $ toDigits nr ) 10 == 0
